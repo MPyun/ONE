169b4e0594ff8f02a24669fcf52cc097d304bbea
MPyun <2322352641@qq.com>
2025-08-06 15:00:31 +0800

    1.升级ListView 2.新增图片圆角背景 3.新增仿微信item布局

--- app/src/main/java/com/huacao/one/MainActivity.java
+++ app/src/main/java/com/huacao/one/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_item1);
         
     }
 	public void tolistview(View v){
